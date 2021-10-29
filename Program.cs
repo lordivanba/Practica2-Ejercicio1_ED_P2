0f0cdea4eeadc5da3fb0c3e37ff1baaba5e52a06
lordivanba <ivanbamail@gmail.com>
2021-10-28 23:15:20 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace practica2_ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            string palabraInvertida = "";
+            var pilaLetras = new Stack<char>();
+
+            System.Console.WriteLine("\nIngrese la palabra que desee invertir\n");
+            palabra = Console.ReadLine();
+
+            var listLetras = palabra.ToList();
+            
+            //Se agrega cada letra de la palabra en la pila
+            foreach(var item in listLetras){
+                pilaLetras.Push(item);
+            }
+
+            foreach (var item in pilaLetras)
+            {
+                palabraInvertida = palabraInvertida + item;
+            }
+            
+            System.Console.WriteLine($"Palabra original: {palabra}\nPalabra invertida: {palabraInvertida}");
+            
+        }
+    }
+}
